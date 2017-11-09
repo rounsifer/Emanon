110e3a2bb5e500507d58b1fce22348ce6753d9af
Ronald Rounsifer <ronrounsifer@me.com>
2017-11-09 19:37:38 +0000

    Populate file added.

---
+++ _populate.sql
@@ -0,0 +1,12 @@
+--
+-- TODO POPULATE THE DATABASE WITH EXAMPLES
+--
+insert into customer values (002, 'Hull', 'Nathan', 'HullMania@mail.com', 'reincarNATE', 'IamTHEgreatest', '321 Pineapple, New York, New York')
+insert into customer values (003, 'Joozer', 'Michael', 'theJoozeyOne@mail.com', 'JoozyJoose', 'juiceIgot', '232 Anagram, San Franciso, California')
+insert into customer values (666, 'Copper', 'Pierce', 'theBeach@mail.com', 'CopperB', 'RentOurApts', '10295 48th Ave, Allendale, Michigan')
+insert into customer values (007, 'Bond', 'James', '007leet@secret.mail.com', 'bond', 'shakenNotStirred', '007 Lab, London, England')
+insert into customer values (012, 'Hull', 'Nathan', 'HullMania@mail.com', 'reincarNATE', 'IamTHEgreatest', '321 Pineapple, New York, New York')
+insert into customer values (052, 'Jobs', 'Steve', 'natural_remedies_only@mail.com', 'SoulSurvivor', 'appleisLIFE!', '695 Arastradero Rd, Palo Alto, CA')
+insert into customer values (069, 'Trump', 'Donald', 'THEGREATEST@maga.com', 'America', 'password', '1100 S Ocean Blvd, Palm Beach, FL')
+insert into customer values (001, 'Obama', 'Barack', 'black_mamba_lol@swag.com', 'BlackCaptainAmerica', 'ComeAtMeBro-_-', '1 President Court, Washington, D.C.')
+insert into customer values (002, 'Biden', 'Joe', 'harvard_man@joebiden.com', 'Bucky', 'WeWillWin', '635 W 42nd St, New York, NY')
