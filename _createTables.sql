6880363274b4c8f8677262c9c0124c744f945a0d
ASisouphone <33473173+ASisouphone@users.noreply.github.com>
2017-12-02 23:46:41 -0500

    Add files via upload
    A somewhat completed DDL based on our schema
    Added
    + ShippingAddresses Table
    + Credit Cards Table
    + Comments
    + Many constraints + Foreign Key constraints
    Removed
    - Order History to reduce complexity

--- _createTables.sql
+++ _createTables.sql
@@ -1,15 +1,17 @@
--- DELETE ALL OF THE TABLES IN CASE THEY WERE PREVIOUSLY CREATES
+-- DELETE ALL OF THE TABLES IN CASE THEY WERE PREVIOUSLY CREATED
 --
 DROP TABLE Customer CASCADE CONSTRAINTS;
+DROP TABLE ShippingAddresses CASCADE CONSTRAINTS;
+DROP TABLE CreditCards CASCADE CONSTRAINTS;
 DROP TABLE UserOrder CASCADE CONSTRAINTS;
 DROP TABLE Category CASCADE CONSTRAINTS;
 DROP TABLE Product CASCADE CONSTRAINTS;
+DROP TABLE BelongsTo CASCADE CONSTRAINTS;
 DROP TABLE OrderDetails CASCADE CONSTRAINTS;
-DROP TABLE OrderHistory CASCADE CONSTRAINTS;
 DROP TABLE Review CASCADE CONSTRAINTS;
 --
 -- THIS IS THE CUSTOMER ENTITY
--- Still needs: SHIPPING ADDRESS AND CREDIT CARDS ADDED
+--
 CREATE TABLE Customer (
 cID INTEGER PRIMARY KEY,
 lname varchar2(25),
@@ -17,14 +19,47 @@ fname varchar2(25),
 email varchar2(25),
 username varchar2(25),
 password varchar2(25),
-billingAddress varchar2(100),
+billingAddress varchar2(100)
+);
+--
+-- Customer Shipping Addresses
+--
+Create Table ShippingAddresses (
+cID INTEGER,
+shippingAddress varchar2(100),
+--
+-- shIC1: Both customers and shipping addresses are stored
+CONSTRAINT shIC1 PRIMARY KEY (cID, shippingAddress),
+-- shIC2: Customers can have multiple shipping addresses
+CONSTRAINT shIC2 FOREIGN KEY (cID) REFERENCES Customer(cID)
+  ON DELETE CASCADE
+);
+--
+-- Customer Credit Cards
+--
+Create Table CreditCards (
+cID INTEGER,
+creditCard varchar2(100),
+--
+-- ccIC1: Both customers and credit cards are stored
+CONSTRAINT ccIC1 PRIMARY KEY (cID, creditCard),
+-- ccIC2: Customers can have multiple credit cards
+CONSTRAINT ccIC2 FOREIGN KEY (cID) REFERENCES Customer(cID)
+            ON DELETE CASCADE
 );
 --
 -- THIS IS THE ORDER ENTITY
 --
 CREATE TABLE UserOrder (
-oID INTEGER PRIMARY KEY,
-totalCost DECIMAL
+uoID INTEGER PRIMARY KEY,
+cID INTEGER,
+totalCost DECIMAL,
+oDate DATE,
+oTime TIMESTAMP,
+--
+-- oIC1: Only customers place orders
+CONSTRAINT oIC1 FOREIGN KEY (cID) REFERENCES Customer(cID)
+            ON DELETE CASCADE
 );
 --
 -- THIS IS THE CATEGORY ENTITY
@@ -46,42 +81,82 @@ price DECIMAL,
 weight DECIMAL
 );
 --
+-- Category of Products Table
+--
+CREATE TABLE BelongsTo (
+pID INTEGER,
+CategoryID INTEGER,
+--
+-- btIC1: The product and its category are stored
+CONSTRAINT btIC1 PRIMARY KEY (CategoryID, pID),
+-- btcIC2: Many categories can be assigned to a product
+CONSTRAINT btIC2 FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID)
+            ON DELETE CASCADE,
+-- btcIC3: Many products can be assigned to a category
+CONSTRAINT btIC3 FOREIGN KEY (pID) REFERENCES Product(pID)
+            ON DELETE CASCADE
+);
+--
 -- THIS IS THE ORDER DETAILS ENTITY
 --
 CREATE TABLE OrderDetails (
 -- If the order id is deleted,
 -- then delete it from the order details
-lineNum INTEGER PRIMARY KEY,
+lineNum INTEGER,
+uoID INTEGER,
+quantity INTEGER,
 shippingMethod varchar2(15),
 shippingCost DECIMAL,
 deliverDate DATE,
-shippingDate DATE
+shippingDate DATE,
+pID Integer,
+--
+-- odIC1: Next the shipping cost for next day shipping must be greater
+-- than or equal to $20.
+CONSTRAINT odIC1 CHECK (NOT(shippingMethod =
+'Next Day' AND shippingCost < 20)),
+-- odIC2: Order Details are unique to a particular order
+CONSTRAINT odIC2 PRIMARY KEY (uoID, lineNum),
+-- odIC3: The order must exist
+CONSTRAINT odIC3 FOREIGN KEY (uoID) REFERENCES UserOrder(uoID)
+            ON DELETE CASCADE,
+-- odIC4: The order details consist of a particular product
+CONSTRAINT odIC4 FOREIGN KEY (pID) REFERENCES Product(pID)
+            ON DELETE CASCADE
 );
 --
 -- THIS IS THE ORDER HISTORY WEAK ENTITY
 --
-CREATE TABLE OrderHistory (
-orderDate DATE,
-orderTime TIMESTAMP,
-PRIMARY KEY(orderTime, orderDate)
-);
+-- CREATE TABLE OrderHistory (
+-- cID INTEGER,
+-- orderDate DATE,
+-- orderTime TIMESTAMP,
+--
+--
+-- PRIMARY KEY(orderTime, orderDate)
+-- );
 --
 -- THIS IS THE REVIEW WEAK ENTITY
 --
 CREATE TABLE Review (
-orderDate DATE PRIMARY KEY,
+revDate DATE,
+cID INTEGER,
+pID INTEGER,
 userComment varchar2(500),
-rating INTEGER
+rating INTEGER,
+-- rIC1: Ratings are between and 1 and 5 inclusive
+CONSTRAINT revIC1 CHECK (NOT(rating < 1 AND rating > 5)),
+-- rIC2: Customers can make multiple reviews for a product, but only on
+-- different dates
+CONSTRAINT revIC2 PRIMARY KEY (cID, pID, revDate),
+-- rIC3: A review is unique to a customer
+CONSTRAINT revIC3 FOREIGN KEY (cID) REFERENCES Customer(cID)
+            ON DELETE CASCADE,
+-- rIC4: A review is unique to a product
+CONSTRAINT revIC4 FOREIGN KEY (pID) REFERENCES Product(pID)
+            ON DELETE CASCADE
 );
 --
--- ADDING FOREIGN KEYS
---
--- FK for the order detail's entity lineNum attribute
-ALTER TABLE OrderDetails
-ADD FOREIGN KEY (lineNum) references UserOrder(oID)
--- FK for the review entity's orderDate attribute
-ALTER TABLE Review
-ADD FOREIGN KEY (orderDate) references OrderHistory(orderDate)
 --
 --
 -- TODO POPULATE THE DATABASE WITH EXAMPLES
