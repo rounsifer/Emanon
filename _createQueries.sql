af3258a8ca4c0a5dbcdf8ebb93a8d75c7c3478bc
ASisouphone <33473173+ASisouphone@users.noreply.github.com>
2017-12-04 23:47:24 -0500

    Add files via upload
    Made simplified version of certain queries
    Fixed some variable names to work with new changes to DDL
    Edited comments

--- _createQueries.sql
+++ _createQueries.sql
@@ -4,71 +4,70 @@
 SPOOL createQueries-a.out
 SET ECHO ON
 
--- Joining 4 relations --
--- Finds the last name, email, and shipping date for all products purchased in quantities > 1
+-- Joining 4 relations -- Q1
+-- Finds the last name, email, and shipping date for all products purchased in quantities greater than 2
 -- Order by last name
-SELECT U.lname, U.email
+SELECT U.lname, U.email, D.shippingDate
 FROM Customer U, OrderDetails D, Product P, UserOrder O
-WHERE U.cID = O.cID AND D.uoID = O.uoID AND 
-      P.pID =  D.pID AND D.quantity > 1
+WHERE U.cID = O.cID AND D.uoID = O.uoID AND
+      P.pID =  D.pID AND D.quantity > 2
 ORDER BY U.lname;
 
--- Self Join --
--- For products with the same cost, finds the pID, category and cost of each product
-SELECT P1.pID, P2.pID, T1.cname, T2.cname, P1.price, P2.price
-FROM Product P1, Product P2, Category T1, Category T2, BelongsTo B1, BelongsTo B2
-WHERE P1.price = P2.price AND P1.pID = B1.pID AND
-      P2.pID = B2.pID AND T1.CategoryID = B1.CategoryID AND 
-      T2.CategoryID = B2.CategoryID;
-
--- Union --
--- Find the pID and pname of every product that has the same price OR is in the same category
-SELECT P1.pID, P1.pname
+-- Self Join -- Q2
+-- Finds the product ID, name and cost of products with the same price
+-- No repeats
+SELECT P1.pID, P2.pID, P1.price, P2.price, P1.pname, p2.pname
 FROM Product P1, Product P2
-WHERE P1.price != P2.price
-UNION
-SELECT P1.pID, P1.pname
-FROM Product P1, Product P2, Category C1, Category C2, BelongsTo B1, BelongsTo B2
-WHERE P1.pID = B1.pID AND B1.CategoryID = C1.CategoryID AND
-	  P2.pID = B2.pID AND B2.CategoryID = C2.CategoryID AND
-	  C2.CategoryID = C1.CategoryID;
+WHERE P1.price = P2.price AND P1.pid < p2.pid;
 
--- SUM --
--- Find the customer name and total cost of all products order by James Bond
-SELECT U.fname, U.lname, SUM(P.price)
-FROM Customer U, Product P, UserOrder O, OrderDetails D
-WHERE U.fname = 'James' AND U.lname = 'Bond' AND
-      U.cID = O.cID AND P.pID = D.pID AND
-      D.uoID = O.uoID
-GROUP BY U.fname, U.lname;
+-- Union -- Q3
+-- Finds the product ID of all computer products that do not have reviews
+Select P.pid
+FROM Product P, BelongsTo B, Category C
+WHERE P.pid = B.pid AND B.categoryID = C.categoryID AND
+      C.cname = 'Computers'
+MINUS
+Select pid
+FROM Review;
 
--- GROUP BY, HAVING, ORDER BY --
--- Find the last name and CID of every customer who has ordered more than 2 products from the 'Office' Category
+-- AVG -- Q4
+-- Finds the product ID, product name and average rating for all the products reviewed
+-- Order by product ID
+Select P.pid, P.pname, AVG(R.rating) "Average Rating"
+FROM Product P, Review R
+WHERE P.pid = R.pid
+GROUP BY P.pid, P.pname
+ORDER BY P.pid;
+
+-- GROUP BY, HAVING, ORDER BY -- Q5
+-- Finds the last name and ID of every customer(s) who has ordered more than 2 products from the 'Food' Category
 -- Order by customer last name
-SELECT U.lname, U.cID, COUNT(*)
+SELECT U.lname, U.cID, COUNT(*) "Food Count"
 FROM Customer U, Product P, UserOrder O, OrderDetails D, BelongsTo B, Category T
 WHERE U.cID = O.cID AND D.uoID = O.uoID AND
       D.pID = P.pID AND P.pID = B.pID AND
-      B.CategoryID = T.CategoryID
+      B.CategoryID = T.CategoryID AND T.cname = 'Food'
 GROUP BY U.lname, U.cID
 HAVING COUNT(*) > 2
 ORDER BY U.lname;
 
--- CORRELATED SUBQUERY --
--- Find the customers who have not made a review
+-- CORRELATED SUBQUERY -- Q6
+-- Finds customer ID and last name of the customers who have not made a review
 SELECT C.cid, C.lname
 FROM Customer C
 WHERE NOT EXISTS (Select *
                   FROM Review R
                   WHERE R.cid = C.cid);
--- NON-CORRELATED SUBQUERY --
--- Find all the products that don't have reviews
-SELECT P.pid, P.name
+
+-- NON-CORRELATED SUBQUERY -- Q7
+-- Finds the product ID and product name for all the products that don't have reviews
+SELECT P.pid, P.pname
 FROM Product P
 WHERE P.pid NOT IN (SELECT R.pid
                     FROM Review R);
--- RELATION DIVISION --
--- Find the customer(s) who have reviewed ALL the award winning products
+
+-- RELATION DIVISION -- Q8
+-- Find the customer ID and last name of the customer(s) who have reviewed ALL the award winning products
 SELECT C.cid, C.lname
 FROM Customer C
 WHERE NOT EXISTS ((Select A.pid
@@ -79,27 +78,21 @@ WHERE NOT EXISTS ((Select A.pid
                    FROM Review R
                    WHERE C.cid = R.cid));
 
--- OUTER JOIN --
--- Find the customers who ordered computers and also their reviews if they have one
--- SELECT C.cid, C.lname, R.Description
--- FROM Customer C, UserOrder O, OrderDetails D, BelongsTo B, Category C, Review R
--- WHERE C.cid = O.cid AND O.oid = D.oid AND D.pid = P.pid AND P.pid = B.pid AND
---       B.categoryID = B.categoryID;
-
--- Find the line num and order ID for all orders and products included in that order detail
+-- OUTER JOIN -- Q9
+-- Find the line number and order ID for all orders and products included in that order detail
 SELECT D.lineNum, D.uoID, P.pID, P.pname
-FROM OrderDetails D LEFT OUTER JOIN Product P ON D.pID = P.PID;
+FROM OrderDetails D LEFT OUTER JOIN Product P ON D.pID = P.PID
+WHERE pname = 'Apple Apple';
 
-
--- RANK QUERY --
+-- RANK QUERY -- Q10
 -- Find the rank of $1000 among the product pricing from largest to smallest
 SELECT Rank(1000) Within Group (Order by price DESC) "Pricing Rank"
-From Product;
+FROM Product;
 
--- TOP-N Query --
--- Find 3 most expensive products
-SELECT pid, name, price
+-- TOP-N Query -- Q11
+-- Find the 3 most expensive products
+SELECT pid, pname, price
 FROM (SELECT *
       FROM Product
-      ORDER BY price)
-Where ROWNUM < 4;  
+      ORDER BY price DESC)
+WHERE ROWNUM < 4;
